c27af7fc39acd91460aa0f3cd0405f9ff88a5920
TiagoTNX <tiagotnx@gmail.com>
2022-01-31 22:33:02 -0300

    Aulas 3, 4 e 5

---
+++ TypeScript - parte 1 Evoluindo seu JavaScript/app/models/negociacoes.ts
@@ -0,0 +1,13 @@
+import { Negociacao } from "./negociacao.js";
+
+export class Negociacoes {
+    private negociacoes: Negociacao[] = [];
+
+    adicionar(negociacao: Negociacao){
+        this.negociacoes.push(negociacao);
+    }
+
+    lista(): readonly Negociacao[] {
+        return this.negociacoes;
+    }
+}
