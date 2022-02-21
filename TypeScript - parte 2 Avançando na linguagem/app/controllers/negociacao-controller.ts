4108b3c50e917706e61cbcd52418bbc15bbaca69
TiagoTNX <tiagotnx@gmail.com>
2022-02-21 17:39:37 -0300

    Aula 4

--- TypeScript - parte 2 Avançando na linguagem/app/controllers/negociacao-controller.ts
+++ TypeScript - parte 2 Avançando na linguagem/app/controllers/negociacao-controller.ts
@@ -9,7 +9,7 @@ export class NegociacaoController {
     private inputQuantidade: HTMLInputElement;
     private inputValor: HTMLInputElement;
     private negociacoes = new Negociacoes();
-    private negociacoesView = new NegociacoesView('#negociacoesView');
+    private negociacoesView = new NegociacoesView('#negociacoesView', true);
     private mensagemView = new MensagemView('#mensagemView');
     private readonly SABADO = 6;
     private readonly DOMINGO = 0;
@@ -22,7 +22,11 @@ export class NegociacaoController {
     }
 
     public adiciona(): void {
-        const negociacao = this.criaNegociacao();
+        const negociacao = Negociacao.criaDe(
+            this.inputData.value,
+            this.inputQuantidade.value,
+            this.inputValor.value
+        );
         if (!this.ehDiaUltil(negociacao.data)) {
             this.mensagemView.update('Apenas negociações em dias úteis são aceitas')
             return;
@@ -34,14 +38,6 @@ export class NegociacaoController {
 
     private ehDiaUltil(data: Date) {
         return data.getDay() > DiaDaSemana.DOMINGO && data.getDay() < DiaDaSemana.SABADO
-    }
-
-    private criaNegociacao(): Negociacao {
-        const exp = /-/g;
-        const date = new Date(this.inputData.value.replace(exp, ','));
-        const quantidade = parseInt(this.inputQuantidade.value);
-        const valor = parseFloat(this.inputValor.value);
-        return new Negociacao(date, quantidade, valor);
     }
 
     private limparFormulario(): void {
