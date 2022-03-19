6e902cf25bf69c357e644a2c17e22896a5e3c906
TiagoTNX <tiagotnx@gmail.com>
2022-03-19 02:22:46 -0300

    Adicionando página do FAQ

--- Next JS/01-nextjs-course-aula2.3/pages/faq.js
+++ Next JS/01-nextjs-course-aula2.3/pages/faq.js
@@ -1,7 +1,11 @@
-import Link from "../src/components/Link";
-import PageTitle from "../src/components/PageTitle";
+// faq.js
+import FAQScreen from "../src/screens/FAQScreen";
+
+export default FAQScreen;
 
 export async function getStaticProps() {
+    console.log("Em modo DEV, sempre roda! A cada acesso");
+    console.log("Roda SOMENTE em build time");
     const FAQ_API_URL =
         "https://gist.githubusercontent.com/omariosouto/0ceab54bdd8182cbd1a4549d32945c1a/raw/578ad1e8e5296fa048e3e7ff6b317f7497b31ad9/alura-cases-faq.json";
     const faq = await fetch(FAQ_API_URL)
@@ -11,27 +15,11 @@ export async function getStaticProps() {
         .then((resposta) => {
             return resposta;
         });
-    return {
-        props: { faq }, // will be passed to the page component as props
-    };
-}
 
-export default function FAQPage({ faq }) {
-    return (
-        <div>
-            <PageTitle>FAQ - Alura Cases Campanha</PageTitle>
-            <h1>Alura Cases - Páginas de Perguntas FAQ</h1>
-            <Link href="/">Ir para a home</Link>
-            <ul>
-                {faq.map(({ answer, question }) => (
-                    <li key={question}>
-                        <article>
-                            <h2>{question}</h2>
-                            <p>{answer}</p>
-                        </article>
-                    </li>
-                ))}
-            </ul>
-        </div>
-    );
+    return {
+        props: {
+            qualquercoisa: "que eu passar aqui",
+            faq,
+        },
+    };
 }
