e09699dc56334e07bfc9ce0f61f189a1c5b57cce
TiagoTNX <tiagotnx@gmail.com>
2022-06-21 00:38:37 -0300

    Aula 1

---
+++ Angular/Angular parte 1 - produtividade e organização com framework SPA/alurapic/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  description = 'Flores';
+  url =
+    'https://img.freepik.com/fotos-gratis/arranjo-lindo-de-papel-de-parede-de-flores_23-2149057015.jpg?w=2000';
+  title = 'alurapic';
+}
